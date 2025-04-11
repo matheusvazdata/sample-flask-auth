3914dd53b1c43f100ca86a99ccbc4e9813d72db7
Matheus Vaz <matheusvaz.data@gmail.com>
2025-04-11 01:48:49 -0300

    Creating the user.py file with the User class

--- models/user.py
+++ models/user.py
@@ -1,7 +1,11 @@
-from app import db
+from database import db
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     # id (int), username (text), password (text)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.nome}>"
