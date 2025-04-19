452793739d8fc67756d1e48a3cd2b81a5d3e9ebc
Matheus Vaz <matheusvaz.data@gmail.com>
2025-04-18 22:29:37 -0300

    Updating Database Settings from SQLite to MySQL

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager, login_user, current_user, logout_user, log
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://admin:admin123@127.0.0.1:3307/flask-crud"
 
 login_manager = LoginManager()  # Inicializa o gerenciador de login
 db.init_app(app)  # Inicializa o banco de dados com a aplicação Flask
